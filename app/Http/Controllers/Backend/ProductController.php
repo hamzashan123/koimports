0f12f5d830b14b3aad5efc6a14b90b949e3dc1eb
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2023-06-14 17:22:37 +0500

    dsad

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -42,9 +42,9 @@ class ProductController extends Controller
         Product::truncate();
         DB::statement("SET foreign_key_checks=1");
 
-        DB::statement("SET foreign_key_checks=0");
-        Category::truncate();
-        DB::statement("SET foreign_key_checks=1");
+        // DB::statement("SET foreign_key_checks=0");
+        // Category::truncate();
+        // DB::statement("SET foreign_key_checks=1");
         
         Excel::import(new ProductsImport,request()->file('file'));
         
